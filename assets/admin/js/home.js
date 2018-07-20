f83721875d9826dc6525378c60f3a6ab59634a8b
Nicolas Ormeno <normeno@digevo.com>
2018-07-20 10:56:39 -0400

    Add Assets

---
+++ assets/admin/js/home.js
@@ -0,0 +1,32 @@
+hljs.initHighlightingOnLoad(); // Init Highlight
+
+(function($) {
+    $( "textarea[name*='insertion-sort']" ).bind('input propertychange', function () {
+        const previewId = '#' + $(this).prop('id') + '-preview';
+        $(previewId).text($(this).val());
+
+        // Refresh Hihlight
+        hljs.initHighlighting.called = false;
+        hljs.initHighlighting();
+    });
+
+    $('#insertion-sort-admin-form').submit(function(e){
+        e.preventDefault();
+
+        const data = $(this).serialize();
+
+        $.post(
+            ajaxurl + '?action=insertion_sort_admin_form',
+            data,
+            function( resp ) {
+                if (resp.updated === 'undefined') {
+                    return false;
+                }
+
+                const msg = (resp.updated > 0) ? [resp.updated + ' items updated','success'] : ['0 items updated', 'warning'];
+
+                $.notify(msg[0], msg[1]);
+            }
+        );
+    });
+})( jQuery );
