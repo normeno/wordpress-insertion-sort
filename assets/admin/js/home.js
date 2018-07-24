b3ae96e396a2cfa6d691c275bd2764158bc3d125
Nicolas Ormeno <normeno@digevo.com>
2018-07-24 12:14:11 -0400

    save improve

--- assets/admin/js/home.js
+++ assets/admin/js/home.js
@@ -15,7 +15,26 @@ hljs.initHighlightingOnLoad(); // Init Highlight
                     return false;
                 }
 
-                const msg = (resp.updated > 0) ? [resp.updated + ' items updated','success'] : ['0 items updated', 'warning'];
+                let msg = [];
+
+                if (resp.updated > 0) {
+                    msg = ['Updated ' + resp.updated + ' items','success'];
+
+                    $('*[id*=insertion-sort-]:visible').each(function() {
+                        if ( !$(this).prop('id').includes('preview')
+                            && ($(this).prop('id').includes('-js') || $(this).prop('id').includes('-css')) ) {
+                            const elementId = '#' + $(this).prop('id');
+                            const previewId = elementId + '-preview';
+
+                            $(previewId).text( $(elementId).val() );
+                        }
+                    });
+
+                    hljs.initHighlighting.called = false;
+                    hljs.initHighlighting();
+                } else {
+                    msg = ['0 items updated', 'warning'];
+                }
 
                 $.notify(msg[0], msg[1]);
             }
